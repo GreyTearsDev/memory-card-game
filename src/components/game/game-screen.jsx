0cc70024556c26692462da23d2e12937014b8e7f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-19 21:33:01 +0100

    feat: Implement GameScreen component

---
+++ src/components/game/game-screen.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Logo from "../logo";
+import Card from "./card";
+
+export default function GameScreen({ gifs, setEndGame, highestScore, setHighestScore }) {
+  const [score, setScore] = useState(0);
+
+  const handleClick = (clicked, setClicked) => {
+    if (clicked) {
+      if (score > highestScore) setHighestScore(score);
+      return setEndGame(true); // causes a rerender
+    }
+
+    setScore(prev => prev + 1);
+    setClicked(true);
+
+    if (score === 10) {
+      if (score > highestScore) setHighestScore(score);
+      return setEndGame(true);
+    }
+  };
+
+  return (
+    <div className="game-screen">
+      <header>
+        <Logo />
+        <div>
+          <p>Score: {score}</p>
+          <p>Highest Score: {highestScore}</p>
+        </div>
+      </header>
+      <main className="card-container">
+        {gifs && gifs.map(gif => {
+          return <Card key={gif.id} src={gif.images["480w_still"].url} onClick={handleClick} />;
+        })}
+      </main>
+    </div>
+  );
+}
+
+GameScreen.propTypes = {
+  gifs: PropTypes.array,
+  setEndGame: PropTypes.func,
+  highestScore: PropTypes.number,
+  setHighestScore: PropTypes.func,
+};
