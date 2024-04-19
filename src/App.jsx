91ca712c545280f9091415cec5fe03b96c99d4ca
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-19 22:21:43 +0100

    refactor: Update App component to fetch GIFs from Giphy API and integrate GameScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import GameScreen from "./components/game/game-screen";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [highestScore, setHighestScore] = useState(0);
+  const [endGame, setEndGame] = useState(false);
+  const [gifs, setGifs] = useState(null);
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=Lfozesrhn1cf7UcN6t4bp1rib0jkXtJ1&q=cat&limit=10`;
+
+  useEffect(() => {
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(response => setGifs(response.data));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GameScreen gifs={gifs} setEndGame={setEndGame} highestScore={highestScore} setHighestScore={setHighestScore} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
