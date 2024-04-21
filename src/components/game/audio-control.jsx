425b6ca4f23b8f0a655cc69659d12fd0f05295c3
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-21 18:21:28 +0100

    refactor: Refactor audio control component for consistency with audio context
    updated the AudioControl component to use handleMusic and handleSFX functions directly instead of relying on an audio context object. Removed the unused audio prop. Also updated the component to use the musicIsOn and sfxAreOn flags directly for button styling and text.

--- src/components/game/audio-control.jsx
+++ src/components/game/audio-control.jsx
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types";
-export default function AudioControl({ audio }) {
-  const handleMusicVolume = (e) => audio.updateMusicVolume(e.target.value / 100);
-  const handleSFXVolume = (e) => audio.updateSFXVolume(e.target.value / 100);
+export default function AudioControl({ handleMusic, handleSFX, musicIsOn, sfxAreOn }) {
   return (
     <>
       <div>
-        <input type="range" min={0} max={100} id="music" onChange={handleMusicVolume} />
-        <label htmlFor="music">Music</label>
-      </div>
-      <div>
-        <input type="range" min={0} max={100} id="sfx" onChange={handleSFXVolume} />
-        <label htmlFor="sfx">Sfx</label>
+        <button className={musicIsOn ? "btn--on" : "btn--off"} onClick={handleMusic}>
+          Music: {musicIsOn ? "ON" : "OFF"}
+        </button>
+        <button className={musicIsOn ? "btn--on" : "btn--off"} onClick={handleSFX}>
+          Sfx: {sfxAreOn ? "ON" : "OFF"}
+        </button>
       </div>
     </>
   );
 }
 
 AudioControl.propTypes = {
-  audio: PropTypes.object,
+  handleMusic: PropTypes.func,
+  handleSFX: PropTypes.func,
+  musicIsOn: PropTypes.bool,
+  sfxAreOn: PropTypes.bool,
 };
