8ff2308682f0c4d78065ec6eb472198fdaae146c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-21 14:53:58 +0100

    refactor: Add game state management and audio control to App component
    modified the App component to include state management for the game (startGame and endGame) and added functionality to handle starting, restarting, and ending the game. Also, integrated the audio context generated by GenerateAudioContext to control game music and sound effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
+import EndScreen from "./components/end-menu/end-screen";
 import GameScreen from "./components/game/game-screen";
+import StartScreen from "./components/start-menu/start-screen";
 import "./App.css";
+import GenerateAudioContext from "./util/audio";
 
 function App() {
   const [highestScore, setHighestScore] = useState(0);
   const [endGame, setEndGame] = useState(false);
+  const [startGame, setStartGame] = useState(false);
   const [gifs, setGifs] = useState(null);
+
+  const audio = GenerateAudioContext();
   const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=Lfozesrhn1cf7UcN6t4bp1rib0jkXtJ1&q=cat&limit=10`;
 
   useEffect(() => {
@@ -14,10 +20,29 @@ function App() {
       .then(response => setGifs(response.data));
   }, []);
 
+  const handleStartGame = () => {
+    setStartGame(true);
+    audio.music.dreamers.play();
+  };
+
+  const handleRestartGame = () => {
+    setStartGame(true);
+    setEndGame(false);
+  };
+
+  const handleGameOver = (score) => {
+    if (score > highestScore) setHighestScore(score);
+    setStartGame(false);
+    setEndGame(true);
+  };
   return (
-    <>
-      <GameScreen gifs={gifs} setEndGame={setEndGame} highestScore={highestScore} setHighestScore={setHighestScore} />
-    </>
+    <div>
+      {!startGame && !endGame && <StartScreen onGameStart={handleStartGame} />}
+      {startGame && !endGame && (
+        <GameScreen gifs={gifs} highestScore={highestScore} onGameOver={handleGameOver} audio={audio} />
+      )}
+      {!startGame && endGame && <EndScreen onGameRestart={handleRestartGame} />}
+    </div>
   );
 }
 
