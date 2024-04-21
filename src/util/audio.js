5d33257d9e5828939b4f8105fd6f7cb1e694fed5
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-21 14:48:33 +0100

    refactor: Refactor audio file paths in 'GenerateAudioContext()'
    refactored audio file paths in GenerateAudioContext to use the correct paths relative to the public folder

--- src/util/audio.js
+++ src/util/audio.js
@@ -1,4 +1,4 @@
-import {Howl, Howler} from "howler"
+import {Howl} from "howler"
 
 export default function GenerateAudioContext() {
   let sfxVolume = 0.5;
@@ -6,34 +6,34 @@ export default function GenerateAudioContext() {
 
   const sfx = {
     highScore: new Howl({
-      src: ['../assets/sfx/highscore.mp3'],
+      src: ['../../public/sfx/highscore.mp3'],
       volume: sfxVolume,
     }),
     gameover: new Howl({
-      src: ['../assets/sfx/gameover.mp3'],
+      src: ['../../public/sfx/gameover.mp3'],
       volume: sfxVolume,
     }),
     success: new Howl({
-      src: ['../assets/sfx/success.mp3'],
+      src: ['../../public/sfx/success.mp3'],
       volume: sfxVolume,
     }),
     click: new Howl({
-      src: ['../assets/sfx/click.mp3'],
+      src: ['../../public/sfx/click.mp3'],
       volume: sfxVolume,
     }),
     cheers1: new Howl({
-      src: ['../assets/sfx/cheers1.mp3'],
+      src: ['../../public/sfx/cheers1.mp3'],
       volume: sfxVolume,
     }),
     cheers2: new Howl({
-      src: ['../assets/sfx/cheers2.mp3'],
+      src: ['../../public/sfx/cheers2.mp3'],
       volume: sfxVolume,
     }),
   }
   
   const music = {
     dreamers: new Howl({
-      src: ['../assets/music/Dreamers.mp3'],
+      src: ['../../public/music/Dreamers.mp3'],
       loop: true,
       volume: musicVolume,
     })
