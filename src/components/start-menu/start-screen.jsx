d2bb307e0497386229bac6cb54ce96bf08d86311
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-22 19:27:52 +0100

    refactor: Refactor start-screen.jsx to simplify structure and adjust class names

--- src/components/start-menu/start-screen.jsx
+++ src/components/start-menu/start-screen.jsx
@@ -4,23 +4,23 @@ import Logo from "../logo";
 export default function StartScreen({ onGameStart }) {
   return (
     <div className="start-screen">
-      <div className="start-screen__menu">
-        <div className="start-screen__menu__logo-wrapper">
-          <h3>Welcome to</h3>
-          <Logo />
-        </div>
-        <button type="button" onClick={onGameStart}>Start Game</button>
-        <div className="credits">
-          <p>
-            Created by <a href="https://github.com/GreyTearsDev">Tirso Samalungo (GreyTearsDev)</a>
-          </p>
+      <div className="start-screen__logo-wrapper">
+        <h3>Welcome to</h3>
+        <Logo />
+      </div>
+
+      <button type="button" onClick={onGameStart}>Start Game</button>
+
+      <div className="credits">
+        <p>
+          Created by <a href="https://github.com/GreyTearsDev">Tirso Samalungo (GreyTearsDev)</a>
+        </p>
+        <div>
+          <p>Powered by</p>
           <div>
-            <p>Powered by</p>
-            <div>
-              <a href="https://giphy.com">
-                <img src="/assets/icons/giphy-logo.png"></img>
-              </a>
-            </div>
+            <a href="https://giphy.com">
+              <img src="/assets/icons/giphy-logo.png"></img>
+            </a>
           </div>
         </div>
       </div>
