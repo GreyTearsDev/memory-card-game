30ccfd9b9de05eb1423c03a8118f4da53b60315c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-22 07:27:16 +0100

    refactor: Add stars and scores to EndScreen component
    - updated the EndScreen component to display stars based on the current final score and show the highest score.
    - added PropTypes validation for 'highestScore' and 'currentFinalScore'.

--- src/components/end-menu/end-screen.jsx
+++ src/components/end-menu/end-screen.jsx
@@ -1,9 +1,21 @@
 import PropTypes from "prop-types";
 
-export default function EndScreen({ onGameRestart }) {
+export default function EndScreen({ onGameRestart, highestScore, currentFinalScore }) {
+  const getStars = () => {
+    let stars = "";
+    for (let i = 0; i < 5; i++) {
+      (i < (currentFinalScore / 2)) ? stars += "⭐️" : stars += "☆";
+    }
+    return stars;
+  };
   return (
     <>
       <h1>Game over</h1>
+      <div>
+        <h1>{getStars()}</h1>
+        <h4>Score: {currentFinalScore}</h4>
+      </div>
+      <p>HighestScore: {highestScore}</p>
       <button type="button" onClick={onGameRestart}>Restart game</button>
     </>
   );
@@ -11,4 +23,6 @@ export default function EndScreen({ onGameRestart }) {
 
 EndScreen.propTypes = {
   onGameRestart: PropTypes.func,
+  highestScore: PropTypes.number,
+  currentFinalScore: PropTypes.number,
 };
