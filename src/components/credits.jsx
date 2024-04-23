fb186a81fedc390b54ecc4ea54027d53bdda3905
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-23 20:27:12 +0100

    refactor: Refactor markup related to credits into is own component

---
+++ src/components/credits.jsx
@@ -0,0 +1,19 @@
+export default function Credits() {
+  return (
+    <div className="credits">
+      <div className="credits__github">
+        <p>
+          Created by <a href="https://github.com/GreyTearsDev">Tirso Samalungo (GreyTearsDev)</a>
+        </p>
+      </div>
+      <div className="credits__giphy">
+        <p>Powered by</p>
+        <div>
+          <a href="https://giphy.com">
+            <img src="/assets/icons/giphy-logo.png"></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
